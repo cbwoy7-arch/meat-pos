59eeb7e7c6b3f20d58e202585005b7333d4c65f4
cbwoy7-arch <cbwoy7@gmail.com>
2026-07-11 14:06:17 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,8 +1,10 @@
-const CACHE = 'meat-pos-v15';
+const CACHE = 'meat-pos-v16';
 const FILES = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)).then(() => self.skipWaiting()));
+  // cache:'reload' skips the HTTP cache so a new SW never installs stale files
+  e.waitUntil(caches.open(CACHE).then(c =>
+    c.addAll(FILES.map(f => new Request(f, {cache: 'reload'})))).then(() => self.skipWaiting()));
 });
 self.addEventListener('activate', e => {
   e.waitUntil(caches.keys().then(keys =>
